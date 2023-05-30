f94b51ea0cb0912be22d1cbfa5702ea96a7ae09a
Jose Ignacio Licona Orduña <jos.licona11@gmail.com>
2023-05-29 18:25:42 -0600

    :sparkles: se agrego una nueva seccion e iconos

--- src/TravelAgency.jsx
+++ src/TravelAgency.jsx
@@ -84,9 +84,8 @@ export const TravelAgency = ()=>{
             <div className="card">
               <img src="https://images.pexels.com/photos/1450353/pexels-photo-1450353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..."/>
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">La boquilla beach</h5>
+                <p><i class="fa-solid fa-location-dot"></i> <small class="text-body-secondary">Mazunte</small></p>
               </div>
             </div> 
           </div>
@@ -94,9 +93,8 @@ export const TravelAgency = ()=>{
             <div className="card">
               <img src="https://images.pexels.com/photos/1174732/pexels-photo-1174732.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..."/>
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">Sayulita beach</h5>
+                <p><i class="fa-solid fa-location-dot"></i> <small class="text-body-secondary">Cancun</small></p>
               </div>
             </div> 
           </div>
@@ -105,10 +103,35 @@ export const TravelAgency = ()=>{
               <img src="https://images.pexels.com/photos/3601425/pexels-photo-3601425.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..."/>
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <p><i class="fa-solid fa-location-dot"></i> <small class="text-body-secondary">Cancun</small></p>
               </div>
             </div> 
+          </div>
+        </div>
+        <div className="row mt-5" style={{ marginBottom:'15px' }}>
+          <div className="col-3">
+            <div className="card text-center">
+              <h3 style={{ lineHeight:"90px" }} className="display-4 text-info">24+</h3>
+              <p className="text-body-secondary">Cities</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card text-center">
+              <h3 style={{ lineHeight:"90px" }} className="display-4 text-info">700+</h3>
+              <p className="text-body-secondary">Place</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card text-center">
+              <h3 style={{ lineHeight:"90px" }} className="display-4 text-info">200+</h3>
+              <p className="text-body-secondary">Hotel</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card text-center">
+              <h3 style={{ lineHeight:"90px" }} className="display-4 text-info">2K+</h3>
+              <p className="text-body-secondary">Review</p>
+            </div>
           </div>
         </div>
       </div>      
