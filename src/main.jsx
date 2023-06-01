1fcaca283d1c2d83fc54921c8a4613f4299e48d5
Jose Ignacio Licona Orduña <jos.licona11@gmail.com>
2023-06-01 16:28:29 -0600

    :zap: Se organizaron las carpetas y se creo el componente nav

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { TravelAgency } from './TravelAgency';
+import { DestinyDetail } from './pages/DestinyDetail';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TravelAgency />
+    <DestinyDetail />
   </React.StrictMode>,
 );
